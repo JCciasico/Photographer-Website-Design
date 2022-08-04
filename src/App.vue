e532db2b6947672f1ddd4e1452143008cfd67ebc
JCCiasico <carlociasico01@gmail.com>
2022-08-04 13:43:03 +0800

    chore: added feature section

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,9 @@
     <div class="services-section">
       <ServicesSection/>
     </div>
+    <div class="feature-section">
+      <FeatureSection/>
+    </div>
   </div>
 </template>
 
@@ -20,6 +23,7 @@ import IntroSection from './components/IntroSection.vue';
 import WelcomeSection from './components/WelcomeSection.vue';
 import ArtSection from './components/ArtSection.vue';
 import ServicesSection from './components/ServicesSection.vue';
+import FeatureSection from './components/FeatureSection.vue';
 
 export default {
   name: 'App',
@@ -28,7 +32,8 @@ export default {
     IntroSection,
     WelcomeSection,
     ArtSection,
-    ServicesSection
+    ServicesSection,
+    FeatureSection
 }
 }
 </script>
@@ -43,6 +48,12 @@ export default {
 .intro-section{
   background: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), 
 	url("./assets/alex-knight-958bi4L1N9s-unsplash.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.feature-section{
+  background: linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), 
+	url("./assets/highway-1jpg.jpg");
   background-repeat: no-repeat;
   background-size: cover;
 }
---
+++ src/components/FeatureSection.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <div class="container">
+        <div class="row text-center d-flex justify-content-center">
+            <div class="col-8 feature-body">
+                <p id="feature">Featured project</p>
+                <span>Restless dreams</span>
+                <p id="title">Dark alley and silent hills</p>
+                <button>Follow me</button>
+            </div>
+        </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Philosopher:wght@400;700&display=swap');
+
+.feature-body{
+    padding: 8rem 0;
+}
+span{
+    font-family: 'Philosopher', sans-serif;
+    font-weight: 700;
+    font-size: 3rem;
+    padding: 1.5rem 0;
+    color: #dfe4ea;
+    border-bottom: 1px solid #a4b0be;
+}
+#title{
+    padding: 3rem;
+    color: #dfe4ea;
+    font-family: 'Philosopher', sans-serif;
+}
+#feature{
+    color: #dfe4ea;
+    font-family: 'Philosopher', sans-serif;
+}
+button{
+    padding: 0.8rem;
+    width: 200px;
+    background:none;
+    border: 3px solid #dfe4ea;
+    color: #dfe4ea;
+    font-family: 'Philosopher', sans-serif;
+}
+button:hover{
+    background:none;
+    color: #00fefc;
+    border: 3px solid #00fefc;
+}
+</style>
