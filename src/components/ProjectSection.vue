9d6b9e724afd1810e3519fd3b8d26a4e668c62b5
JCCiasico <carlociasico01@gmail.com>
2022-08-06 10:31:26 +0800

    chore: fixed responsiveness

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -2,18 +2,18 @@
   <section>
     <div class="container">
         <div class="row d-flex justify-content-center">
-            <div class="col-9">
+            <div class="col-lg-9 col-md-9 col-sm-12">
                 <div class="row">
-                    <div class="col-6 border text-center img-1">
-                        <h1>Tell me you dont want</h1>
+                    <div class="col-lg-6 col-md-12 img-1">
+                        <h2>Tell me you dont want</h2>
                     </div>
-                    <div class="col-6 border">
+                    <div class="col-lg-6 col-md-12">
                         <div class="row">
-                            <div class="col-12 border img-2">
-                                <h1>Nights & Colours</h1>
+                            <div class="col-12 img-2">
+                                <h2>Nights & Colours</h2>
                             </div>
-                            <div class="col-12 border img-3">
-                                <h1>Ontario Lake</h1>
+                            <div class="col-12 img-3">
+                                <h2>Ontario Lake</h2>
                             </div>
                         </div>
                     </div>
@@ -34,25 +34,77 @@ export default {
 @import url('https://fonts.googleapis.com/css2?family=Philosopher:wght@400;700&display=swap');
 
 .img-1{
-    background: url("../assets/miss-zhang-jz0xIgcwyL0-unsplash.jpg");
+    background: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),  url("../assets/miss-zhang-jz0xIgcwyL0-unsplash.jpg");
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
-    padding: 10rem 0;
+    padding: 15rem 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 .img-2{
-    background: url("../assets/juan-felipe-Kk8mEQAoIpI-unsplash.jpg");
+    background: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),  url("../assets/juan-felipe-Kk8mEQAoIpI-unsplash.jpg");
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
-    padding: 5rem 0;
+    padding: 7.5rem 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 .img-3{
-    background: url("../assets/claudia-chiavazza-N9vsB6OEeKM-unsplash.jpg");
+    background: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),  url("../assets/claudia-chiavazza-N9vsB6OEeKM-unsplash.jpg");
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
-    padding: 5rem 0;
+    padding: 7.5rem 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+h2{
+    font-family: 'Philosopher', sans-serif;
+    color: white;
+    font-weight: 700;
+
+}
+h2:hover{
+    transform: scale(1.035);
+    cursor: pointer;
+    color: #00fefc;
+}
+@media(max-width:991px){
+    .img-1{
+    background: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),  url("../assets/miss-zhang-jz0xIgcwyL0-unsplash.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    padding: 6rem 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.img-2{
+    background: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),  url("../assets/juan-felipe-Kk8mEQAoIpI-unsplash.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    padding: 6rem 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.img-3{
+    background: linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),  url("../assets/claudia-chiavazza-N9vsB6OEeKM-unsplash.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    padding: 6rem 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 }
 
 </style>
