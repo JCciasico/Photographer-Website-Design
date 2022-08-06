189e60d8ee6356d4006ccd3aca4d662bf2a2c8e1
JCCiasico <carlociasico01@gmail.com>
2022-08-06 10:12:22 +0800

    chore: added h1 tags

--- src/components/ProjectSection.vue
+++ src/components/ProjectSection.vue
@@ -4,16 +4,16 @@
         <div class="row d-flex justify-content-center">
             <div class="col-9">
                 <div class="row">
-                    <div class="col-6 border img-1">
-                        
+                    <div class="col-6 border text-center img-1">
+                        <h1>Tell me you dont want</h1>
                     </div>
                     <div class="col-6 border">
                         <div class="row">
                             <div class="col-12 border img-2">
-                                
+                                <h1>Nights & Colours</h1>
                             </div>
                             <div class="col-12 border img-3">
-
+                                <h1>Ontario Lake</h1>
                             </div>
                         </div>
                     </div>
@@ -31,6 +31,7 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Philosopher:wght@400;700&display=swap');
 
 .img-1{
     background: url("../assets/miss-zhang-jz0xIgcwyL0-unsplash.jpg");
@@ -47,7 +48,7 @@ export default {
     padding: 5rem 0;
 }
 .img-3{
-    background: url("../assets/juan-felipe-Kk8mEQAoIpI-unsplash.jpg");
+    background: url("../assets/claudia-chiavazza-N9vsB6OEeKM-unsplash.jpg");
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
