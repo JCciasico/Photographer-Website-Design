2db8dd52fa323a7049d07ceb2c51d669bb76e665
JCCiasico <carlociasico01@gmail.com>
2022-08-06 09:46:56 +0800

    chore: added project section

---
+++ src/components/ProjectSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <div class="container">
+        
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.container{
+    width: 100vw;
+	height: 100vh;
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+	grid-template-rows: 70px 1fr 1fr 70px;
+	gap: 20px;
+	background-color: darkgray;
+}
+
+</style>
